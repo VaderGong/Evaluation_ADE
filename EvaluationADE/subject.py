5acc70936555f058b2a1eb07708890665075d18d
gejq21 <gejq21@mails.tsinghua.edu.cn>
2023-11-17 15:11:51 +0800

    dd

--- EvaluationADE/subject.py
+++ EvaluationADE/subject.py
@@ -38,6 +38,7 @@ class subject:
         calculate acceleration
         :return: list of acceleration
         '''
+        print('acceleration')
         pass
 
     def acceleration_rate(self)->list:
